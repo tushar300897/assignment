c9909d1bd120e815eb7a855623042bb6aa8c4e26
tushar sachdeva <tusharsachdeva@tushars-MacBook-Air.local>
2021-09-26 23:50:25 +0530

    Resolved slider width issue.

--- web/themes/custom/mytheme/js/global.js
+++ web/themes/custom/mytheme/js/global.js
@@ -1,22 +1,70 @@
-(function ($, Drupal) {
-  Drupal.behaviors.conferences = {
-    attach(context) {
-      const $context = $(context);
-      $context.find('#slider-tabs .nav-item a').attr("data-toggle", "tab");
-      $('#slider-tabs li:first a').addClass('active');
-      $context.find('.slider__three').slick({
+(function($, Drupal) {
+Drupal.behaviors.conferences = {  };
+$(document).ready(function(){
+  $('#slider-tabs .nav-item a').attr("data-toggle", "tab");
+  $('#slider-tabs li:first a').addClass('active');
+  $(".nav-link-recommended-conferences").click(function() {
+    $('.slider__one').slick({
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      nextArrow: $('.slider-wrapper .slick-next'),
+      prevArrow: $('.slider-wrapper .slick-previous'),
+      responsive: [{
+          breakpoint: 1800,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+
+          },
+        },
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          },
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            arrows: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            prevArrow: false,
+            nextArrow: false,
+            dots: true,
+            autoplay: true,
+          },
+        },
+      ],
+    });
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+      $('.slider__one').slick('setPosition');
+    })
+  });
+
+
+  $(".nav-link-past-conferences").click(function() {
+    $('.slider__two').slick({
       infinite: true,
       speed: 300,
       slidesToShow: 3,
       nextArrow: $('.slider-wrapper .slick-next'),
       prevArrow: $('.slider-wrapper .slick-previous'),
-      responsive: [
-        {
+      responsive: [{
           breakpoint: 1800,
           settings: {
             slidesToShow: 3,
             slidesToScroll: 1,
-            
+
           },
         },
         {
@@ -38,106 +86,63 @@
           settings: {
             slidesToShow: 1,
             slidesToScroll: 1,
+            autoplay: false,
             prevArrow: false,
             nextArrow: false,
             dots: true,
-            autoplay: false,
           },
         },
       ],
     });
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+      $('.slider__two').slick('setPosition');
+    })
+  });
 
-    $context.find(".nav-link-past-conferences").click(function(){
-      $context.find('.slider__two').slick({
-        infinite: true,
-        speed: 300,
-        slidesToShow: 3,
-        nextArrow: $('.slider-wrapper .slick-next'),
-        prevArrow: $('.slider-wrapper .slick-previous'),
-        responsive: [
-          {
-            breakpoint: 1800,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
-              
-            },
-          },
-          {
-            breakpoint: 1200,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
-            },
-          },
-          {
-            breakpoint: 1025,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1,
-            },
-          },
-          {
-            breakpoint: 993,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              autoplay: false,
-              prevArrow: false,
-              nextArrow: false,
-              dots: true,
-            },
-          },
-        ],
-      });
-    });
+  $('.slider__three').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    nextArrow: $('.slider-wrapper .slick-next'),
+    prevArrow: $('.slider-wrapper .slick-previous'),
+    responsive: [{
+        breakpoint: 1800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
 
-    $(".nav-link-recommended-conferences").click(function(){
-      $('.slider__one').slick({
-        infinite: true,
-        speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        nextArrow: $('.slider-wrapper .slick-next'),
-        prevArrow: $('.slider-wrapper .slick-previous'),
-        responsive: [
-          {
-            breakpoint: 1800,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
-              
-            },
-          },
-          {
-            breakpoint: 1200,
-            settings: {
-              slidesToShow: 3,
-            },
-          },
-          {
-            breakpoint: 1025,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-            },
-          },
-          {
-            breakpoint: 992,
-            settings: {
-              arrows: false,
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              prevArrow: false,
-              nextArrow: false,
-              dots: true,
-              autoplay: true,
-            },
-          },
-        ],
-      });
-    });
-    },
-  };
+        },
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 993,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          prevArrow: false,
+          nextArrow: false,
+          dots: true,
+          autoplay: false,
+        },
+      },
+    ],
+  });
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+    $('.slider__three').slick('setPosition');
+  })
 
+});
 })(jQuery, Drupal);
